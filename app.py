3ac488d8405be04f0d8751f75082473d43829066
Regina Steffen <reginasteffen@live.com>
2024-06-09 12:33:18 -0500

    home page / index route created

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secretkey'
 
 debug = DebugToolbarExtension(app)
+
+RESPONSES = []
+
+@app.route('/')
+def index():
+    '''home page'''
+    return render_template('home.html', survey=satisfaction_survey)
