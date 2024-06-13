7167052465fa0635aa3db58e401b3757dd4d80be
Regina Steffen <reginasteffen@live.com>
2024-06-13 10:43:32 -0500

    added message flashing + redirect if user tries to access questions out of order

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -26,8 +26,13 @@ def start_survey():
 @app.route('/questions/<id>')
 def show_question(id):
     '''show user current question'''
+    if int(id) != len(RESPONSES):
+        #redirect to current question if user tries to access questions out of order
+        flash(f'INVALID QUESTION ID: {id}')
+        return redirect(f'/questions/{len(RESPONSES)}')
     id = len(RESPONSES)
     if len(RESPONSES) == len(survey.questions):
+        # when survey is complete, show user 'thank you' page
         return redirect('/completed')
     return render_template('questions.html', survey=survey, questions=survey.questions[id], id=id)
 
